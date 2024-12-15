060c8bdbbde198f39e6c6711fb7891f9807c83ae
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-15 11:02:19 +0700

    types

--- prj/pokedex/src/components/TypeCard.jsx
+++ prj/pokedex/src/components/TypeCard.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { pokemonTypeColors } from '../utils'
 
-export function TypeCard() {
+export default function TypeCard(props) {
+  const {type} = props
   return (
-    <div>TypeCard</div>
+    <div className='type-tile' 
+      style={{color: pokemonTypeColors?.[type]?.color, 
+      background: pokemonTypeColors?.[type]?.background}}>
+      <p>{type}</p>
+    </div>
   )
 }
