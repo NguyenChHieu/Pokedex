67f44da373be4977a8cec3c0aef7bb047ec1ca52
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-18 17:44:31 +0700

    add search

--- prj/pokedex/src/components/SideNav.jsx
+++ prj/pokedex/src/components/SideNav.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import { useState } from 'react'
 import {first151Pokemon, getFullPokedexNumber } from "../utils"
 
-export default function SideNav() {
+export default function SideNav(props) {
+  const {pokemon, setPokemon} = props
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+    // if full pokedex number includes search value, true
+    if (toString(getFullPokedexNumber(eleIndex)).includes(searchValue)) {
+      return true
+    }
+    if (ele.toLowerCase().includes(searchValue)) {
+      return true
+    }
+    return false
+  })
+
   return (
     <nav>
-      <div>
+      <div className={"header"}>
         <h1 className='text-gradient'>Pokédex</h1>
       </div>
-      <input/>
-      {first151Pokemon.map((pokemon, pokemonIndex) => {
+      <input value={searchValue} onChange={(e) => {
+        setSearchValue(e.target.value)
+      }}/>
+      {filteredPokemon.map((pokemon, pokemonIndex) => {
         return (
-          <button className='nav-card ' key={pokemonIndex}>
-            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+          <button onClick={() => {
+            setPokemon(pokemonIndex)
+          }}
+          className={'nav-card ' + (pokemonIndex === pokemon ? ' nav-card-selected': ' ')} key={pokemonIndex}>
+            <p>{getFullPokedexNumber(first151Pokemon.indexOf(pokemon))}</p>
             <p>{pokemon}</p>
           </button>
         )
